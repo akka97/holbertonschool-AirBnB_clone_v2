0f8179eaa71fce6b2d98f40c760dbcf0d4dcfcab
akka97 <akapellari@yahoo.com>
2023-08-04 11:11:53 +0200

    add

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            obj_dict = {}
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    obj_dict[key] = value
+            return obj_dict
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +53,15 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Method deletes"""
+        if obj is not None:
+            storage = self.all()
+            for key, value in storage.items():
+                if value == obj:
+                    del storage[key]
+                    break
