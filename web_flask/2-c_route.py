7a18e4c74009300663e8d9742b92acb2cab0fd11
akka97 <akapellari@yahoo.com>
2023-08-16 03:19:01 -0700

    tasks

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,11 +14,13 @@ def hello_hbnb():
 def hbnb():
     return'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def str_txt(text):
     formatted_text = text.replace('_', ' ')
     result = f"C {formatted_text}"
     return result
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
